c0e4450154e0b0d2661197d2dd9eb6cb38257c2c
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2024-08-31 01:26:49 -0300

    config: Subindo base do Projeto

---
+++ localprinter/localprinter/src/main/java/dev/pedrohfreitas/localprinter/LocalPrinterApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrohfreitas.localprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalPrinterApplication.class, args);
+	}
+
+}
---
+++ localprinter/localprinter/src/test/java/dev/pedrohfreitas/localprinter/LocalPrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrohfreitas.localprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalPrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
